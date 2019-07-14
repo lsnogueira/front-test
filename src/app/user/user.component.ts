f15ef0f3b74380814b3e2d34cb877993aa0f20d3
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-07-14 13:00:51 -0300

    regiser layout

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user.component.html',
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['../../assets/scss/components/_user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     document.body.classList.add('bg-user');
   }
-
 }
